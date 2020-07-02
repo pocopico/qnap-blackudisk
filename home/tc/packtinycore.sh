d1ee2a26faca167877fde3b4ac171eeabdb84436
root <pasafiogos@gmail.com>
2020-07-02 16:10:06 +0300

    changes changes

--- home/tc/packtinycore.sh
+++ home/tc/packtinycore.sh
@@ -34,11 +34,13 @@ echo "Creating Temporary folder"
 mkdir -p  $WORKING_DIR/$TINYCOREFOLDER/home/tc
 mkdir -p $WORKING_DIR/$TINYCOREFOLDER/tmp
 
+echo "Removing local initrd folder, size is limited  ..."
+rm -rf $WORKING_DIR/initrd
+
 
 echo "Copying contents for temp FOLDER"
 cp -rp /home/tc/* $WORKING_DIR/$TINYCOREFOLDER/home/tc
 cp -rp /tmp/tce $WORKING_DIR/$TINYCOREFOLDER/tmp/
-rm -rf $WORKING_DIR/$TINYCOREFOLDER/$TINYCOREFOLDER
 
 echo "Creating tinycore image with the contents of folder $TINYCOREFOLDER "
 
